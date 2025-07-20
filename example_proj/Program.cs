2df9ea38d20144631c8f47dda787cb21be1da9ca
jules <jules.white1@protonmail.com>
2025-07-21 00:49:32 +0100

    Added lots of functionality, and added a chessboard example

--- example_proj/Program.cs
+++ example_proj/Program.cs
@@ -1,22 +1,27 @@
 ﻿using static SvgLib.SvgColour;
-using System;
 using SvgLib;
 
 Console.WriteLine("Serializing SVG");
 
-var svg = new SvgLib.Svg();
+var svg = new SvgLib.Svg(500, 500);
 
-svg.Shapes.AddRange([
-    new Rectangle()
-        .Position(50, 50)
-        .Size(100, 20)
-        .Background(BLACK),
+List<Shape> shapes = [];
 
-    new Line()
-        .Position(10, 10)
-        .Size(100, 10)
-        .Border(BLACK),
-]);
+Enumerable.Range(0, 8)
+    .SelectMany(row =>
+        Enumerable.Range(0, 8)
+            .Select(col =>
+                new TextBox(col * 48, row * 40, $"{(char)('A' + col)}{8 - row}")
+                    .Background((row + col) % 2 == 0 ? WHITE : BLACK)
+                    .Foreground((row + col) % 2 == 0 ? BLACK : WHITE)
+                    .Rounded(false)
+            )
+    )
+    .Select(tb => tb.Shapes())
+    .ToList()
+    .ForEach(shapes.AddRange);
+
+svg.Shapes.AddRange(shapes);
 
 svg.Serialize("Resources/example.svg");
 
