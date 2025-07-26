9d9a6c565ffd5ecd74dd1c7458461d7d417f0dc7
juleswhi <jules.white1@protonmail.com>
2025-07-27 00:29:56 +0100

    upadte build.sh and add pert chart

---
+++ SvgLib/Charts/Pertt.cs
@@ -0,0 +1,8 @@
+using static SvgLib.SvgColour;
+namespace SvgLib;
+
+public class PerttChart {
+    public static Svg Draw(Task[] tasks, string title) {
+        return new SvgLib.Svg();
+    }
+}
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-dotnet run --project example_proj/example_proj.csproj --no-restore && sxiv example.svg
+dotnet run --project example_proj/example_proj.csproj --no-restore --configuration Release && sxiv example.svg
